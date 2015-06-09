3445c381be5a70333720b275d72e33b9fe500352
John WALTER <john.walter@wassa.fr>
2015-06-09 14:42:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+h1. Wassa Data Load Bundle
---
+++ Wassa/DataLoaderBundle/WassaDataLoaderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wassa\DataLoaderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WassaDataLoaderBundle extends Bundle
+{
+}
